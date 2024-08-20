ae1d606cb3808a3c9d1f2fb58e15243908bfbb57
zhihengdu <weicheng199610@gmail.com>
2024-08-20 16:12:56 +0800

    feat:根据ID更新租约状态

--- lease/web/web-admin/src/main/java/com/du/lease/web/admin/controller/lease/LeaseAgreementController.java
+++ lease/web/web-admin/src/main/java/com/du/lease/web/admin/controller/lease/LeaseAgreementController.java
@@ -1,8 +1,10 @@
 package com.du.lease.web.admin.controller.lease;
 
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.du.lease.common.result.Result;
+import com.du.lease.model.entity.ApartmentInfo;
 import com.du.lease.model.entity.LeaseAgreement;
 import com.du.lease.model.enums.LeaseStatus;
 import com.du.lease.web.admin.service.LeaseAgreementService;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class LeaseAgreementController {
     @Autowired
     private LeaseAgreementService service;
+
     @Operation(summary = "保存或修改租约信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody LeaseAgreement leaseAgreement) {
@@ -32,7 +35,7 @@ public class LeaseAgreementController {
     @GetMapping("page")
     public Result<IPage<AgreementVo>> page(@RequestParam long current, @RequestParam long size, AgreementQueryVo queryVo) {
         Page<AgreementVo> agreementVoPage = new Page<>(current, size);
-        IPage<AgreementVo> result =  service.pageAgreement(agreementVoPage,queryVo);
+        IPage<AgreementVo> result = service.pageAgreement(agreementVoPage, queryVo);
         return Result.ok(result);
     }
 
@@ -46,12 +49,17 @@ public class LeaseAgreementController {
     @Operation(summary = "根据id删除租约信息")
     @DeleteMapping("removeById")
     public Result removeById(@RequestParam Long id) {
+        service.removeById(id);
         return Result.ok();
     }
 
     @Operation(summary = "根据id更新租约状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam LeaseStatus status) {
+        LambdaUpdateWrapper<LeaseAgreement> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(LeaseAgreement::getId, id);
+        wrapper.set(LeaseAgreement::getStatus, status);
+        service.update(wrapper);
         return Result.ok();
     }
 
